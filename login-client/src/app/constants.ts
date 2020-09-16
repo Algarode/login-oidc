02fd66af2c6329b970ab34c17657b7d047a69fda
Edwin Lambregts <edwin.lambregts0@gmail.com>
2020-09-16 16:54:05 +0200

    AuthService and Constants

---
+++ login-client/src/app/constants.ts
@@ -0,0 +1,9 @@
+export class Constants {
+    public static clientRoot = 'http://localhost:4200/';
+
+    public static apiRoot = 'https://securingangularcoursev2-api-unsecure.azurewebsites.net/api/';
+    public static stsAuthority = 'https://securingangularcoursev2-sts.azurewebsites.net/';
+
+    public static clientId = 'spa-client';
+
+}
--- login-client/src/app/core/auth-service.component.ts
+++ login-client/src/app/core/auth-service.component.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserManager, User } from 'oidc-client';
+import { Constants } from '../constants';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -7,6 +8,16 @@ export class AuthService {
     private _userManager: UserManager;
     private _user: User;
 
-    constructor() { }
+    constructor() {
+        const stsSettings = {
+            authority: Constants.stsAuthority,
+            client_id: Constants.clientId,
+            redirect_uri: `${Constants.clientRoot}signin-callback`,
+            scope: 'openid profile projects-api',
+            response_type: 'code',
+            post_logout_redirect_uri: `${Constants.clientRoot}signout-callback`
+        };
+        this._userManager = new UserManager(stsSettings);
+    }
     
 }
